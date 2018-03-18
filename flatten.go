918469d8cbdbedd003b7feb9920f22531ea42c37
wolfeidau <mark.wolfe@versent.com.au>
2018-03-18 20:34:49 +1100

    fix(flatten) Adjust internal function name to be more descriptive.

--- flatten.go
+++ flatten.go
@@ -8,17 +8,17 @@ func JoinWithDot(ks []string) string { return strings.Join(ks, ".") }
 // Flatten take a hierarchy and flatten it using the tokenizer supplied
 func Flatten(m map[string]interface{}, tokenizer func([]string) string) map[string]interface{} {
 	var r = make(map[string]interface{})
-	flattenrecurse(m, []string{}, func(ks []string, v interface{}) {
+	flattenRecursive(m, []string{}, func(ks []string, v interface{}) {
 		r[tokenizer(ks)] = v
 	})
 	return r
 }
 
-func flattenrecurse(m map[string]interface{}, ks []string, cb func([]string, interface{})) {
+func flattenRecursive(m map[string]interface{}, ks []string, cb func([]string, interface{})) {
 	for k, v := range m {
 		newks := append(ks, k)
 		if newm, ok := v.(map[string]interface{}); ok {
-			flattenrecurse(newm, newks, cb)
+			flattenRecursive(newm, newks, cb)
 		} else {
 			cb(newks, v)
 		}
